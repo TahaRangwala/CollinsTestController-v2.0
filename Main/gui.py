e62283568ae460b0c5a261a772bc3d7ae017da8b
TahaRangwala <tahamr@vt.edu>
2021-01-26 17:05:44 -0500

    added to gui

--- Main/gui.py
+++ Main/gui.py
@@ -15,28 +15,36 @@ def runGUI():
                     [sg.Output(size=(60,10), key='-OUTPUT-')], 
                     [sg.Button('Refresh Connections')]]
 
+    #Test Configuration
+    tempLayout2 = [[sg.Text('Test:'), sg.Radio('Mix Spur Test', "RADIO1", default=True), sg.Radio('P1dB Test', "RADIO1"), sg.Radio('PinvPout Test', "RADIO1")],
+                    [sg.Text('JSON File:'), sg.InputText(key='-IN2-')],
+                    [sg.Button('Add')],
+                    [sg.Output(size=(60,10), key='-OUTPUT-')], 
+                    [sg.Button('Refresh Configurations')]]
+
     #Running Tests
-    tempLayout2 = [[sg.Button('Mixer Spur Test'), sg.Button('P1dB Test'), sg.Button('PinvPout Test')]]
+    tempLayout3 = [[sg.Button('Mixer Spur Test'), sg.Button('P1dB Test'), sg.Button('PinvPout Test')]]
 
     #Plot Settings
-    tempLayout3 = [[sg.Text('Title'), sg.InputText(key='-IN3-')],
+    tempLayout4 = [[sg.Text('Title'), sg.InputText(key='-IN3-')],
                 [sg.Text('X-Label'), sg.InputText(key='-IN4-')],
                 [sg.Text('Y-Label'), sg.InputText(key='-IN5-')],
                 [sg.Button('Apply Changes')]]
 
-    layout = [[sg.Frame(layout=tempLayout1, title='Equipment Connections', element_justification='c')],
+    layout = [[sg.Frame(layout=tempLayout1, title='Equipment Connections', element_justification='c'), sg.Frame(layout=tempLayout2, title='Test Configuration', element_justification='c')],
             [sg.Text('')],
-            [sg.Frame(layout=tempLayout2, title='Runnable Tests', element_justification='c')],
+            [sg.Frame(layout=tempLayout3, title='Runnable Tests', element_justification='c')],
             [sg.Text('')],
-            [sg.Frame(layout=tempLayout3, title='Plot Settings', element_justification='c')]]
+            [sg.Frame(layout=tempLayout4, title='Plot Settings', element_justification='c')],
+            [sg.Button('Close', size =(10, 2))]]
 
-    window = sg.Window('Universal PA Test Controller v2.0', layout, element_justification='c', size=(650, 590))
+    window = sg.Window('Universal PA Test Controller v2.0', layout, element_justification='c', size=(950, 630))
 
     #Loop running while GUI is open
     i = -1
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Exit':
+        if event == sg.WIN_CLOSED or event == 'Close':
             break
         elif event == 'Add':
             i += 1
