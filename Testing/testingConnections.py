35cbc23bc4bb26c9812584c1c830bcb323d24bd9
TahaRangwala <tahamr@vt.edu>
2021-01-29 13:38:51 -0500

    connections

--- Testing/testingConnections.py
+++ Testing/testingConnections.py
@@ -1,1 +1,10 @@
 #Testing
+import pyvisa
+rm = pyvisa.ResourceManager('@py')
+print(rm.list_resources())
+theResource = 'TCPIP0::192.168.1.140::INSTR'
+my_instrument = rm.open_resource(theResource)
+print(my_instrument)
+my_instrument.read_termination = '\n'
+my_instrument.write_termination = '\n'
+print(my_instrument.query("*IDN?"))
