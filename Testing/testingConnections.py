ec3fb99051729611fd3c58c4ae458ae822c7a483
TahaRangwala <tahamr@vt.edu>
2021-01-29 19:03:27 -0500

    testing pyvisa functions

--- Testing/testingConnections.py
+++ Testing/testingConnections.py
@@ -2,7 +2,7 @@
 import pyvisa
 rm = pyvisa.ResourceManager('@py')
 print(rm.list_resources())
-theResource = 'TCPIP0::192.168.1.140::INSTR'
+theResource = 'TCPIP0::192.168.1.101::INSTR'
 my_instrument = rm.open_resource(theResource)
 print(my_instrument)
 my_instrument.read_termination = '\n'
