6db9207a7b2c98f7e16b48e345415b6629535df2
TahaRangwala <supertaha@gmail.com>
2021-03-18 17:55:05 -0400

    Fully Complete and Commented Code

--- otherTest.py
+++ otherTest.py
@@ -1,7 +1,13 @@
-from tests import Run_Tests
-import matplotlib.pyplot as plt
-import numpy as np
+#File Description: This .py file contains the Other_Test class. This is a template class that can be modified for a custom test.
+#Make note of what the other test child classes are doing to get an idea of what a new test could look like
 
+#Required imports
+from tests import Run_Tests#Importing the Run_Tests class from tests.py
+import matplotlib.pyplot as plt#used for plotting graphs
+import numpy as np#used for array operations
+
+#This function returns a list of x values, an array of y values, and an x axis start and stop position based off a
+#set of points passed in, a center frequency, and a frequency span as well
 def parseGetTrace(plotPoints, centerFreq, freqSpan):
     str_data = str(plotPoints)
     str_data = str_data.split(" ", 1)[1]
@@ -14,11 +20,14 @@ def parseGetTrace(plotPoints, centerFreq, freqSpan):
     x = np.arange(start, stop, step)
     return x, data_array, start, stop
 
+#Other_Test class declaration
 class Other_Test(Run_Tests):
-
+    
+    #Constructor for Other_Test
     def __init__(self, name, fileName, title, xlabel, ylabel, freqUnits):
         Run_Tests.__init__(self, name, fileName, title, xlabel, ylabel, freqUnits) 
-
+    
+    #Modifiable run test function that is unique for this child class.
     def runTest(self):
         numCommands = int(self.run['num'])
         if(self.equipmentConnected == False and numCommands <= 0):
@@ -36,12 +45,15 @@ class Other_Test(Run_Tests):
         ax.set_xlabel(self.xLabel)
         ax.set_ylabel(self.yLabel)
         ax.set_title(self.graphTitle)
-
+        
+        #Iterating through all of the commands in the run section of the tests json file associated with this
+        #class
         iterationCount = 0
         commandString = 'cmd'
         firstTime = False
         while(abortTest == False and iterationCount <= 7):
             for i in range(numCommands):
+                #Getting information on the current command
                 currentCommand = commandString + str(i + 1)
                 commandType = self.run[currentCommand]['type']
                 commandSyntax = self.run[currentCommand]['cmd']
@@ -52,10 +64,10 @@ class Other_Test(Run_Tests):
                     if(device.name == equipmentName):
                         fullCommand = str(commandSyntax) + str(commandArgs)
                         if(commandType == 'q'):
-                            if(title == 'Get Trace'):
+                            if(title == 'Get Trace'):#Unique to the Get Trace Command
                                 if(plt.fignum_exists(figNum) and abortTest == False):
                                     try:
-                                        if(firstTime == False):
+                                        if(firstTime == False):#initial graph if first iteration
                                             plotPoints = device.query(fullCommand)
                                             frequency, powerDB, start, stop = parseGetTrace(plotPoints, self.centerFrequency, self.frequencySpan)
                                             ax.set_xlim(start, stop)
@@ -63,7 +75,7 @@ class Other_Test(Run_Tests):
                                             plt.draw()
                                             plt.pause(0.02)
                                             firstTime = True
-                                        else:
+                                        else:#updating graph
                                             plotPoints = device.query(fullCommand)
                                             frequency, powerDB, start, stop = parseGetTrace(plotPoints, self.centerFrequency, self.frequencySpan)
                                             line.set_data(frequency, powerDB)
@@ -85,7 +97,8 @@ class Other_Test(Run_Tests):
                 plt.ioff()
                 plt.show()
                 break
-
+        
+        #If the test is aborted
         if(abortTest == True):
             #self.isConfigured = False
             return False, "Aborted"
